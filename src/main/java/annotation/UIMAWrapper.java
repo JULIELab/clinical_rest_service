cb35ab8c94bcf50d9a6d24faa38f11bf2f1b2c70
hellrich <hellrich.j@gmail.com>
2019-05-06 16:24:43 +0200

    UIMA parameters

--- src/main/java/annotation/UIMAWrapper.java
+++ src/main/java/annotation/UIMAWrapper.java
@@ -3,9 +3,7 @@ package annotation;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 
-import org.apache.uima.UIMAFramework;
 import org.apache.uima.analysis_engine.AnalysisEngine;
-import org.apache.uima.cas.Type;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import pipelines.AutoClosingPipeline;
@@ -15,11 +13,21 @@ import de.julielab.jcore.ae.jnet.uima.EntityAnnotator;
 import de.julielab.jcore.ae.jpos.postagger.POSAnnotator;
 import de.julielab.jcore.ae.jsbd.main.SentenceAnnotator;
 import de.julielab.jcore.ae.jtbd.main.TokenAnnotator;
-import de.julielab.jcore.types.EntityMention;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 
 public class UIMAWrapper {
+
+	private static final Object[] ENTITY_TAGGER_PARAMS = new Object[] {
+			"EntityTypes", "LABEL=de.julielab.jcore.types.EntityMention", //TODO add labels, all should use de.julielab.jcore.types.EntityMention
+			"ExpandAbbreviations", "false", "ConsistencyPreservation", "",
+			"ModelFilename", "TODO" //TODO get real name
+	};
+
+	private static final Object[] POS_TAGGER_PARAMS = new Object[] { "tagset",
+			"full.qualified.name", //TODO get real set
+			"ModelFilename", "TODO" //TODO get real name
+	};
 
 	private final int numThreads;
 	private final int timeout; //TODO: honor!
@@ -49,10 +57,14 @@ public class UIMAWrapper {
 
 	private static AnalysisEngine[] createAnalysisEngines()
 			throws ResourceInitializationException {
-		AnalysisEngine sentenceSplitter = createEngine(SentenceAnnotator.class); //TODO: key-value pairs of additional parameters
-		AnalysisEngine tokenizer = createEngine(TokenAnnotator.class); //TODO: key-value pairs of additional parameters
-		AnalysisEngine posTagger = createEngine(POSAnnotator.class); //TODO: key-value pairs of additional parameters
-		AnalysisEngine entityTagger = createEngine(EntityAnnotator.class); //TODO: key-value pairs of additional parameters
+		AnalysisEngine sentenceSplitter = createEngine(SentenceAnnotator.class,
+				SentenceAnnotator.PARAM_MODEL_FILE, "todo"); //TODO: model name
+		AnalysisEngine tokenizer = createEngine(TokenAnnotator.class,
+				TokenAnnotator.PARAM_MODEL, "todo"); //TODO: model name
+		AnalysisEngine posTagger = createEngine(POSAnnotator.class,
+				POS_TAGGER_PARAMS);
+		AnalysisEngine entityTagger = createEngine(EntityAnnotator.class,
+				ENTITY_TAGGER_PARAMS);
 		return new AnalysisEngine[] { sentenceSplitter, tokenizer, posTagger,
 				entityTagger };
 	}
