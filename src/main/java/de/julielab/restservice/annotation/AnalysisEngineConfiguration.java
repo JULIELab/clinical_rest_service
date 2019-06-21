59af78909e1b0bc95238af87ff809ab10ea8080c
hellrich <hellrich.j@gmail.com>
2019-06-21 09:09:58 +0200

    private constructor, moved array hack-around to readConfiguration

--- src/main/java/de/julielab/restservice/annotation/AnalysisEngineConfiguration.java
+++ src/main/java/de/julielab/restservice/annotation/AnalysisEngineConfiguration.java
@@ -8,31 +8,33 @@ import com.google.gson.Gson;
 
 public class AnalysisEngineConfiguration {
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static AnalysisEngineConfiguration readConfiguration(
 			final String configurationFile) throws Exception {
 		final AnalysisEngineConfiguration config = new Gson().fromJson(
 				Files.readString(Paths.get(configurationFile)),
 				AnalysisEngineConfiguration.class);
+		//fixes mismatch between expected types, avoids modification of tagger classes
+		for (int i = 0; i < config.entityTaggerParams.length; ++i)
+			if (config.entityTaggerParams[i] instanceof ArrayList)
+				config.entityTaggerParams[i] = ((ArrayList) config.entityTaggerParams[i])
+						.toArray(new String[0]);
 		return config;
 	}
 
+	private AnalysisEngineConfiguration() {
+		//should use readConfiguration instead	
+	}
+
 	public boolean usePOS;
+	//Object[] sadly needed by UIMA API 🙄
 	public Object[] entityTaggerParams;
 	public Object[] posTaggerParams;
 	public Object[] sentenceSplitterParams;
-
 	public Object[] tokenizerParams;
 
-	@SuppressWarnings("unchecked")
 	public Object[] getEntityTaggerParams() {
-		//fixes mismatch between expected types, avoids modification of tagger classes
-		final Object[] transformedEntityTaggerParams = entityTaggerParams
-				.clone();
-		for (int i = 0; i < transformedEntityTaggerParams.length; ++i)
-			if (transformedEntityTaggerParams[i] instanceof ArrayList)
-				transformedEntityTaggerParams[i] = ((ArrayList<String>) transformedEntityTaggerParams[i])
-						.toArray(new String[0]);
-		return transformedEntityTaggerParams;
+		return entityTaggerParams;
 	}
 
 	public String getInternalEncoding() {
